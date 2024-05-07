dcb5726491e2ff79a53acdf6a3ed2ff8d2a21e7a
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-05-07 16:47:37 +0300

    feat(title): implement changing page title

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -64,6 +64,18 @@ export default function MovieDetails({
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
