04f5e2cbecf12f3e0e5cebdf702c3523a7d44548
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-05-08 16:42:32 +0300

    refactor(hook): extract searching & fetching movies logic to a custom hook named useMovies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import ErrorMessage from "./components/common/ErrorMessage";
-import KEY from "./components/common/Config";
 import Loader from "./components/common/Loader";
 import Box from "./components/layout/Box";
 import Main from "./components/layout/Main";
@@ -11,13 +10,13 @@ import NumResults from "./components/search/NumResults";
 import Search from "./components/search/Search";
 import WatchedMovieList from "./components/watched/WatchedMovieList";
 import WatchedSummary from "./components/watched/WatchedSummary";
+import { useMovies } from "./hooks/useMovies";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const { movies, isLoading, error } = useMovies(query);
 
   const [watched, setWatched] = useState(function () {
     const storedData = localStorage.getItem("watched");
@@ -45,52 +44,6 @@ export default function App() {
       localStorage.setItem("watched", JSON.stringify(watched));
     },
     [watched]
-  );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError(""); // her data fetch'te error'u sil
-
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      handleCloseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
   );
 
   return (
